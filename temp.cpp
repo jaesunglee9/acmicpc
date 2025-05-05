12a0adf2275e3edd4309c12018752a2ba22b4a76
Jaesung Lee <ecumenepolis@gmail.com>
2025-05-05 20:28:06 +0900

    yah

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,8 @@
 // template for competitive programming
 // also don't forget cout << '\n'
 // don't use c style input 
-#include <bits/stdc++.h>
+#include <cstdlib>
+#include <iostream>
 using namespace std;
 
 const int INF = 1e9 + 7;
